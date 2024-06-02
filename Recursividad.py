09876b0ec948f255c7707b6384b7ef8fa46b40ef
Oswe-gif <jhoanoswaldo72@gmail.com>
2024-06-02 12:59:15 -0500

    Add add recursion exercises

---
+++ Recursividad.py
@@ -0,0 +1,43 @@
+lista1 = [1,2,2]
+#función para sumar elementos de un array sin bucles.
+def suma(array):
+    total=0
+    #caso base -->fin del bucle
+    if(len(array)==1):
+        return array[0]
+    else:
+        #continua el bucle
+        total += array.pop(0)
+        return total + suma(array)
+        
+lista2 = [2,1,3]
+#función para contar elementos de un array sin bucles.
+def contarElementos(array):
+    total =1
+    #caso base
+    if(len(array)==1):
+        return 1
+    else:
+        #continua el bucle
+        array.pop(0)
+        return total + contar(array)
+    
+lista3 = [2,1,3]
+#función para encontrar el maximo de un array sin bucles.
+def maximo(array):
+    #caso base
+    if(len(array)==1):
+        return array[0]
+    else:
+        actual=array.pop(0)
+        if(actual< maximo(array)):
+            return maximo(array)
+        else:
+            return actual
+           
+
+print(suma(lista1))
+print(contar(lista2))
+print(maximo(lista3))
+
+
